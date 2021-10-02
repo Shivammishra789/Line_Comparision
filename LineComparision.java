ef4e01f98120b8cfd7b959344fba686f0b0d08ad
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-02 18:50:26 +0530

    UC:2 check two length are same or not

--- LineComparision.java
+++ LineComparision.java
@@ -8,7 +8,9 @@ public class LineComparision {
 		Scanner endpoints= new Scanner(System.in); 
 		
 		//Taking start and end coordinates of line from user
-		
+
+		System.out.println("Enter coordinates for Line1 ");
+
 		System.out.print("Enter x1 ");  
 		double x1= endpoints.nextInt();
 		
@@ -20,14 +22,34 @@ public class LineComparision {
 		
 		System.out.print("Enter y2 ");  
 		double y2= endpoints.nextInt();
+
+		System.out.println("Enter coordinates for Line2");
+
+ 		System.out.print("Enter x3 ");
+ 		double x3= endpoints.nextInt();
+
+ 		System.out.print("Enter y3 ");
+ 		double y3= endpoints.nextInt();
+
+ 		System.out.print("Enter x4 ");
+ 		double x4= endpoints.nextInt();
+
+ 		System.out.print("Enter y4 ");
+ 		double y4= endpoints.nextInt();
+
 	
-		System.out.println("Entered values are x1="+x1+"; y1="+y1+"; x2="+x2+"; y2="+y2);
+		System.out.println("Line1 coordinates are x1="+x1+"; y1="+y1+"; x2="+x2+"; y2="+y2);
 		
+		 System.out.println("Line2 coordinates are x3="+x3+"; y3="+y3+"; x4="+x4+"; y4="+y4);
 		//Calculating length of line
 		
-		double length=Math.sqrt(Math.pow((x2-x1),2) +Math.pow((y2-y1),2));
+		double length1=Math.sqrt(Math.pow((x2-x1),2) +Math.pow((y2-y1),2));
 		
-		System.out.println("The length of line is "+length);
+		double length2=Math.sqrt(Math.pow((x4-x3),2) +Math.pow((y4-y3),2));
+
+		System.out.println("The length of line is "+length1);
+
+		 System.out.println("The length of line is "+length2);
 	
 	
 	
